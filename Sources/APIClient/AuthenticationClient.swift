0a2576a62588691334ddee599b1b9d4989d65043
D Smailes <david.smailes@gmail.com>
2024-07-27 13:07:03 +0100

    Remove default implementation

--- Sources/APIClient/AuthenticationClient.swift
+++ Sources/APIClient/AuthenticationClient.swift
@@ -14,34 +14,3 @@ public protocol AuthenticationClientProtocol {
     func fetchStoredToken() async throws -> OAuthAccessToken
     
 }
-
-public final class DefaultAuthenticationClient: AuthenticationClientProtocol {
-    
-    private let keychainInteractor = DefaultKeychainInteractor()
-    let authenticationServer: AuthenticationServerConnectionProtocol
-    
-    public init(
-        authenticationServer: AuthenticationServerConnectionProtocol
-    ) {
-        self.authenticationServer = authenticationServer
-    }
-    
-    public func fetchStoredToken() async throws -> OAuthAccessToken {
-        try keychainInteractor.read(withKey: "authToken")
-    }
-    
-    public func requestToken() async throws {
-        let token = try await authenticationServer.getAuthenticationToken()
-        try keychainInteractor.add(token, withKey: "authToken")
-    }
-    
-    public func authenticateRequest(_ request: URLRequest) async throws -> URLRequest {
-        let token = try await fetchStoredToken()
-        
-        var authenticatedRequest = request
-        authenticatedRequest.addValue("Bearer \(token.token)", forHTTPHeaderField: "Authorization")
-        
-        return authenticatedRequest
-    }
-    
-}
