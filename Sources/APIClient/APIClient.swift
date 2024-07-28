d698bf8193d7d192a24d4cf7b5e13ccea1ac3afa
D Smailes <david.smailes@gmail.com>
2024-07-28 14:26:02 +0100

    Allow for query items

--- Sources/APIClient/APIClient.swift
+++ Sources/APIClient/APIClient.swift
@@ -7,40 +7,11 @@ public protocol APIClientProtocol {
     var client: AuthenticationClientProtocol { get }
     var session: URLSession { get }
     
-    func fetch<T: Codable>(type: T.Type, endpoint: EndPointProtocol) async throws -> T
     func fetch<T: Codable>(type: T.Type, endpoint: EndPointProtocol) -> Observable<T>
 }
 
 extension APIClientProtocol {
-    
-    public func fetch<T: Codable>(
-        type: T.Type,
-        endpoint: EndPointProtocol
-    ) async throws -> T {
-        guard let url = URL(string: baseUrl.appending(endpoint.urlSuffix))
-        else { throw APIError.invalidConfiguration }
         
-        var request = URLRequest(url: url)
-        request.timeoutInterval = 15
-        request.httpMethod = endpoint.httpMethod
-        
-        let authenticatedRequest = try client.authenticateRequest(request)
-        
-        let (data, response) = try await session.dataTaskWithURL(authenticatedRequest)
-        
-        guard let httpResponse = response as? HTTPURLResponse
-        else { throw APIError.requestFailed(description: "Invalid response.") }
-        
-        guard 200..<300 ~= httpResponse.statusCode
-        else { throw APIError.requestFailed(description: "Failed with status \(httpResponse.statusCode)") }
-        
-        if let data {
-           return try JSONDecoder().decode(T.self, from : data)
-        } else {
-            throw APIError.decodingFailure
-        }
-    }
-    
     public func fetch<T: Codable>(type: T.Type, endpoint: EndPointProtocol) -> Observable<T> {
         return Observable<T>.create { observer in
             
